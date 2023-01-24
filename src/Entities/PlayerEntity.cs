6952dcc7ba2b4adb9795d645f2dc3573ab3ede00
omega2028 <omega2028.yt@gmail.com>
2023-01-24 21:54:18 +0100

    [#4] Added DbContext and PlayerEntity (fail)

---
+++ src/Entities/PlayerEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    /// <summary>
+    /// This Entity represents a Player in a database.
+    /// </summary>
+    public class PlayerEntity
+    {
+        /// <summary>
+        /// The ID of the player.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The name of the player.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// The image of the player.
+        /// </summary>
+        public string? Image { get; set; }
+    }
+
+}
