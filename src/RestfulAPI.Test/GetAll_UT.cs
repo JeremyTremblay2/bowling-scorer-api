27fafe2c3ffb0e55143be9bfc3b81fe39359c10b
midubois1 <midubois1@IUTCLINFA1906>
2023-01-26 18:17:41 +0100

    [16] Test try

---
+++ src/RestfulAPI.Test/GetAll_UT.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using Services;
+
+namespace RestfulAPI.Test
+{
+    [TestClass]
+    public class GetAll_UT
+    {
+
+        [TestMethod]
+        public void GetAllTest()
+        {
+            // Arrange
+
+            var repository = new Mock<IPlayerService>
+        }
+    }
+}
