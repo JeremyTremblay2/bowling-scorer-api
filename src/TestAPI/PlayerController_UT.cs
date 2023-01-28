48bc05dae8104267c5ccbd7df6f0428f83baedd1
midubois1 <Mickael.DUBOIS@etu.uca.fr>
2023-01-28 09:36:10 +0100

    [#16] EditPlayer tests added

--- src/TestAPI/PlayerController_UT.cs
+++ src/TestAPI/PlayerController_UT.cs
@@ -19,7 +19,7 @@ namespace TestAPI
             _mockPlayerService.Setup(service => service.GetAll())
                 .ReturnsAsync(new List<Player> { new Player("Mickael", "mickael.png") });
             var _mockLoger = new NullLogger<PlayerController>();
-            var controller = new PlayerController(_mockLoger , _mockPlayerService.Object);
+            var controller = new PlayerController(_mockLoger, _mockPlayerService.Object);
 
             //Act
             var result = await controller.GetAll();
@@ -27,7 +27,7 @@ namespace TestAPI
             //Assert
             var okResult = Assert.IsType<OkObjectResult>(result);
             var returnValue = Assert.IsAssignableFrom<IEnumerable<PlayerDTO>>(okResult.Value);
-            Assert.Equal(new List<PlayerDTO>(){ new PlayerDTO { Name = "Mickael", Image = "mickael.png" }}, returnValue);
+            Assert.Equal(new List<PlayerDTO>() { new PlayerDTO { Name = "Mickael", Image = "mickael.png" } }, returnValue);
         }
 
         [Fact]
@@ -138,9 +138,9 @@ namespace TestAPI
             var result = await controller.Add(player);
 
             //Assert
-            var notFoundResult = Assert.IsType<BadRequestObjectResult>(result);
-            Assert.NotNull(notFoundResult.Value);
-            Assert.Equal(notFoundResult.Value, "A player with the same ID already exists");
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.NotNull(badRequestResult.Value);
+            Assert.Equal(badRequestResult.Value, "A player with the same ID already exists");
         }
 
         [Fact]
@@ -168,11 +168,61 @@ namespace TestAPI
             var result = await controller.Add(player);
 
             //Assert
-            var notFoundResult = Assert.IsType<BadRequestObjectResult>(result);
-            Assert.NotNull(notFoundResult.Value);
-            Assert.Equal(notFoundResult.Value, "Failed to add the player (error while saving)");
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.NotNull(badRequestResult.Value);
+            Assert.Equal(badRequestResult.Value, "Failed to add the player (error while saving)");
         }
 
+        [Fact]
+        public async void EditPlayerTest()
+        {
+            //Arrange
+            var _mockPlayerService = new Mock<IPlayerService>();
+            _mockPlayerService.Setup(service => service.EditPlayer(It.IsAny<Player>()))
+                .Returns<Player>(async (player) =>
+                {
+                    if (player.ID == 16)
+                    {
+                        throw new FunctionnalException("The player that you want to edit doesn't exists");
+                    }
+                });
+            var _mockLoger = new NullLogger<PlayerController>();
+            var controller = new PlayerController(_mockLoger, _mockPlayerService.Object);
+            var player = new PlayerDTO { ID = 3, Image = "moi.png", Name = "Moi" };
 
+            //Act
+            var result = await controller.Edit(player);
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult.Value);
+            Assert.Equal(okResult.Value, "Successfuly edited the player id : 3");
+        }
+
+        [Fact]
+        public async void EditPlayerFailedTest()
+        {
+            //Arrange
+            var _mockPlayerService = new Mock<IPlayerService>();
+            _mockPlayerService.Setup(service => service.EditPlayer(It.IsAny<Player>()))
+                .Returns<Player>(async (player) =>
+                {
+                    if (player.ID == 16)
+                    {
+                        throw new FunctionnalException("The player that you want to edit doesn't exists");
+                    }
+                });
+            var _mockLoger = new NullLogger<PlayerController>();
+            var controller = new PlayerController(_mockLoger, _mockPlayerService.Object);
+            var player = new PlayerDTO { ID = 16, Image = "moi.png", Name = "Moi" };
+
+            //Act
+            var result = await controller.Edit(player);
+
+            //Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.NotNull(badRequestResult.Value);
+            Assert.Equal(badRequestResult.Value, "The player that you want to edit doesn't exists");
+        }
     }
 }
