3bf0952ad59ba7a2e094842ecd6a034276305919
mickael.dubois <mickael.dubois@etu.uca.fr>
2023-02-11 17:33:56 +0100

    [#53] Added StatisticsEntity

---
+++ src/Entities/StatisticEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class StatisticEntity
+    {
+        public int ID { get; set; }
+        public int NumberOfVictory { get; set; }
+        public int NumberOfDefeat { get; set; }
+        public int NumberOfGames { get; set; }
+        public int BestScore { get; set; }
+    }
+}
