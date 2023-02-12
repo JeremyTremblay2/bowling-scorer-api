cb82785b0d34309bcb8929ca685ad4b54f342f83
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-02-12 21:57:05 +0100

    [#23] Add custom exceptions for the service.

---
+++ src/Services/StatisticsNotValidException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class StatisticsNotValidException : ArgumentException
+    {
+        public StatisticsNotValidException(string message) : base(message)
+        {
+
+        }
+    }
+}
