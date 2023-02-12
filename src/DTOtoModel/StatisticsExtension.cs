dc0bf8e632211b994d44582d69dbe5fadb97307b
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-02-12 22:07:15 +0100

    [#22] Add a statistics extension class

---
+++ src/DTOtoModel/StatisticsExtension.cs
@@ -0,0 +1,31 @@
+﻿using DTOs;
+using Model.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOtoModel
+{
+    public static class StatisticsExtension
+    {
+        public static StatisticDTO ToDTO(this Statistics statistics)
+        {
+            return new StatisticDTO
+            {
+                ID = statistics.ID,
+                NumberOfDefeat = statistics.NumberOfDefeat,
+                NumberOfVictory = statistics.NumberOfVictory,
+                NumberOfGames = statistics.NumberOfGames,
+                BestScore = statistics.BestScore
+            };
+        }
+
+        public static Statistics ToModel(this StatisticDTO statisticsDTO)
+        {
+            return new Statistics(statisticsDTO.ID, statisticsDTO.NumberOfVictory, 
+                statisticsDTO.NumberOfDefeat, statisticsDTO.NumberOfGames, statisticsDTO.BestScore);
+        }
+    }
+}
