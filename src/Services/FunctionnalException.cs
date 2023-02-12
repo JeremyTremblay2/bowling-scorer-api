5a857967c621ccd3448de4122490724e7cd91215
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-02-12 23:18:47 +0100

    [#22] Add some documentation about the services

--- src/Services/FunctionnalException.cs
+++ src/Services/FunctionnalException.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace DTOs
 {
+    /// <summary>
+    /// A custom exception class that represents a functional error.
+    /// </summary>
     public class FunctionnalException : ArgumentException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FunctionnalException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public FunctionnalException(string? message) : base(message)
         {
         }
--- src/Services/StatisticsNotFoundException.cs
+++ src/Services/StatisticsNotFoundException.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-    public  class StatisticsNotFoundException : ArgumentException
+    /// <summary>
+    /// Represents an error when a Statistics object was not found.
+    /// </summary>
+    public class StatisticsNotFoundException : ArgumentException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StatisticsNotFoundException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public StatisticsNotFoundException(string? message) : base(message)
         {
         }
