f2e69e3a3a24747ba1770a5328bf2122e28521cf
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-02-12 23:27:27 +0100

    [#22] Add documentation on extension methods

--- src/EntityToModel/PlayerExtension.cs
+++ src/EntityToModel/PlayerExtension.cs
@@ -9,8 +9,16 @@ using System.Threading.Tasks;
 
 namespace EntityToModel
 {
+    /// <summary>
+    /// Extension class for Player model.
+    /// </summary>
     public static class PlayerExtension
     {
+        /// <summary>
+        /// Extension method that converts a Player object to a PlayerEntity object.
+        /// </summary>
+        /// <param name="player">The Player object to convert.</param>
+        /// <returns>A PlayerEntity object representing the Player object.</returns>
         public static PlayerEntity ToEntity(this Player player)
         {
             return new PlayerEntity
@@ -22,9 +30,15 @@ namespace EntityToModel
             };
         }
 
+        /// <summary>
+        /// Extension method that converts a PlayerEntity object to a Player object.
+        /// </summary>
+        /// <param name="playerEntity">The PlayerEntity object to convert.</param>
+        /// <returns>A Player object representing the PlayerEntity object.</returns>
         public static Player ToModel(this PlayerEntity playerEntity)
         {
             return new Player(playerEntity.ID, playerEntity.Name, playerEntity.Image /*playerEntity.Statistics.ToModel()*/);
         }
     }
+
 }
--- src/EntityToModel/StatisticsExtension.cs
+++ src/EntityToModel/StatisticsExtension.cs
@@ -9,23 +9,37 @@ using System.Threading.Tasks;
 
 namespace EntityToModel
 {
+    /// <summary>
+    /// Extension class to convert between the Statistics model and its entity representation.
+    /// </summary>
     public static class StatisticsExtension
     {
+        /// <summary>
+        /// Converts a Statistics model to a StatisticsEntity.
+        /// </summary>
+        /// <param name="statistics">The Statistics model to be converted.</param>
+        /// <returns>A StatisticsEntity that represents the Statistics model.</returns>
         public static StatisticsEntity ToEntity(this Statistics statistics)
         {
             return new StatisticsEntity
             {
                 ID = statistics.ID,
-                NumberOfVictory= statistics.NumberOfVictory,
-                NumberOfGames= statistics.NumberOfGames,
-                BestScore= statistics.BestScore,
+                NumberOfVictory = statistics.NumberOfVictory,
+                NumberOfGames = statistics.NumberOfGames,
+                BestScore = statistics.BestScore,
                 NumberOfDefeat = statistics.NumberOfDefeat
             };
         }
 
+        /// <summary>
+        /// Converts a StatisticsEntity to a Statistics model.
+        /// </summary>
+        /// <param name="statisticsEntity">The StatisticsEntity to be converted.</param>
+        /// <returns>A Statistics model that represents the StatisticsEntity.</returns>
         public static Statistics ToModel(this StatisticsEntity statisticsEntity)
         {
             return new Statistics(statisticsEntity.ID, statisticsEntity.NumberOfVictory, statisticsEntity.NumberOfDefeat, statisticsEntity.NumberOfGames, statisticsEntity.BestScore);
         }
     }
+
 }
